9c395e6f744c67c13fc54200f484ab40c250da67
Ryan Chen <b04502132@ntu.edu.tw>
2022-07-06 15:53:04 +0800

    test compile by g++

--- cpp_exec/main.cpp
+++ cpp_exec/main.cpp
@@ -1,12 +1,9 @@
 #include <stdio.h>
+#include "../cpp_lib/hello_world.h"
 
 
 int main(int argc, char *argv[]) 
 {
-    printf("We have %d arguments:\n", argc);
-    for (int i = 0; i < argc; ++i) 
-    {
-        printf("[%d] %s\n", i, argv[i]);
-    }
+    printHelloWorld();
     return 0;
 }
